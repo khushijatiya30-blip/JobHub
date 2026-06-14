869eaff280416240be871564ad63bf2800c4bd4d
khushijatiya <jatiyakhushi206@gmail.com>
2026-06-14 15:20:12 +0530

    fix cors issue

---
+++ jobhub-responsive/backend/src/main/java/com/placement/config/CorsConfige.java
@@ -0,0 +1,23 @@
+package com.placement.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfige {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("https://job-hub-cc5e.vercel.app")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedHeaders("*");
+            }
+        };
+    }
+}
